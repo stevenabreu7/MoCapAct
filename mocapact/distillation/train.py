96cbc639c87427fd5441bd96f22f8007a9ff46e6
Steve Abreu <s.abreu@rug.nl>
2023-08-28 17:45:51 +0200

    training for RNNs (currently without GPU support)

--- mocapact/distillation/train.py
+++ mocapact/distillation/train.py
@@ -20,6 +20,11 @@ from mocapact import utils
 from mocapact.distillation import callbacks
 from mocapact.distillation import dataset
 
+# # avoid gym <> gymnasium conflict
+# import gymnasium
+# import sys
+# sys.modules["gym"] = gymnasium
+
 FLAGS = flags.FLAGS
 
 # Path flags
@@ -56,7 +61,7 @@ flags.DEFINE_bool("keep_hdf5s_open", False, "Whether to keep all HDF5s open (wil
 
 # Model hyperparameters
 config_file = "mocapact/distillation/config.py"
-config_flags.DEFINE_config_file("model", f"{config_file}:mlp_time_index", "Model architecture")
+model_config = config_flags.DEFINE_config_file("model", f"{config_file}:mlp_time_index", "Model architecture")
 flags.DEFINE_list("gpus", None, "GPU configuration (https://pytorch-lightning.readthedocs.io/en/stable/common/trainer.html#gpus)")
 flags.DEFINE_bool("normalize_obs", False, "Whether to normalize the input observation")
 flags.DEFINE_string("load_path", None, "Load path to warm-start")
@@ -80,10 +85,40 @@ flags.mark_flag_as_required("output_root")
 flags.mark_flag_as_required("train_dataset_paths")
 flags.mark_flag_as_required("dataset_metrics_path")
 
+
+def pad_collate(batch):
+    """Create batches from sequences of unequal episode lengths.
+    Padding with zeros.
+    """
+    (xx, yy, zz) = zip(*batch)
+
+    x_lens = [len(x[list(x.keys())[0]]) for x in xx]
+    y_lens = [len(y) for y in yy]
+    z_lens = [len(z) for z in zz]
+
+    assert y_lens == z_lens == x_lens, 'x, y, z lengths must be equal'
+
+    xx_pad = {
+        obs_key: torch.nn.utils.rnn.pad_sequence(
+            [torch.Tensor(x[obs_key]) for x in xx], batch_first=True
+        ) for obs_key in xx[0].keys()
+    }
+    yy_pad = torch.nn.utils.rnn.pad_sequence(
+        [torch.Tensor(y) for y in yy], batch_first=True
+    )
+    zz_pad = torch.nn.utils.rnn.pad_sequence(
+        [torch.Tensor(z) for z in zz], batch_first=True
+    )
+    return xx_pad, yy_pad, zz_pad, x_lens, y_lens, z_lens
+
+
 def main(_):
     output_dir = FLAGS.output_root
     if FLAGS.include_timestamp:
-        output_dir = osp.join(output_dir, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        output_dir = osp.join(output_dir,
+                              datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+
+    is_rnn = 'RnnPolicy' in FLAGS.model.constructor
 
     # Make supervision dataset
     if hasattr(FLAGS.model.config, 'seq_steps'):
@@ -92,7 +127,6 @@ def main(_):
         seq_steps = FLAGS.model.config.block_size
     else:
         seq_steps = 1
-
 
     # Log some stuff (but only in process 0)
     if os.getenv("LOCAL_RANK", "0") == "0":
@@ -112,7 +146,9 @@ def main(_):
         print(FLAGS.train_dataset_paths)
         if FLAGS.val_dataset_paths is not None:
             random.shuffle(FLAGS.val_dataset_paths)
-    train_dataset = dataset.ExpertDataset(
+
+    ds_cls = dataset.ExpertDatasetRNN if is_rnn else dataset.ExpertDataset
+    train_dataset = ds_cls(
         FLAGS.train_dataset_paths,
         observables.MULTI_CLIP_OBSERVABLES_SANS_ID,
         FLAGS.dataset_metrics_path,
@@ -164,21 +200,27 @@ def main(_):
 
     # Make policy to be trained
     model_constructor = utils.str_to_callable(FLAGS.model.constructor)
+    feat_extr_kwargs = dict(observable_keys=FLAGS.model.config.observables,
+                            obs_rms=obs_rms)
     policy = model_constructor(
         train_dataset.full_observation_space,
         train_dataset.action_space,
         ref_steps=train_dataset.ref_steps,
         learning_rate=FLAGS.learning_rate,
-        features_extractor_kwargs=dict(observable_keys=FLAGS.model.config.observables, obs_rms=obs_rms),
+        features_extractor_kwargs=feat_extr_kwargs,
         **FLAGS.model.config
     )
 
+    collate_fn = pad_collate if is_rnn else None
     train_loader = DataLoader(train_dataset, shuffle=True, pin_memory=True,
-                              batch_size=FLAGS.batch_size, num_workers=FLAGS.n_workers)
-    if FLAGS.val_dataset_paths is not None and FLAGS.validation_freq is not None:
-        val_loader = DataLoader(val_dataset, batch_size=FLAGS.batch_size, num_workers=FLAGS.n_workers)
-    else:
-        val_loader = None
+                              batch_size=FLAGS.batch_size,
+                              num_workers=FLAGS.n_workers,
+                              collate_fn=collate_fn)
+    val_loader = None
+    if FLAGS.val_dataset_paths is not None:
+        if FLAGS.validation_freq is not None:
+            val_loader = DataLoader(val_dataset, batch_size=FLAGS.batch_size,
+                                    num_workers=FLAGS.n_workers)
 
     ############
     # Callbacks
@@ -192,7 +234,7 @@ def main(_):
         train_time_interval=timedelta(minutes=FLAGS.save_every_n_minutes)
     )
     train_callbacks.append(last_model_callback)
-    if val_loader is not None: # Validation set callback
+    if val_loader is not None:  # Validation set callback
         train_callbacks.append(pl.callbacks.ModelCheckpoint(
             dirpath=osp.join(output_dir, "eval/validation"),
             filename="best",
@@ -200,15 +242,19 @@ def main(_):
             save_top_k=1,
             every_n_epochs=1
         ))
-    for eval_mode in FLAGS.eval_mode: # Policy evaluation callbacks
+    for eval_mode in FLAGS.eval_mode:  # Policy evaluation callbacks
         is_train_dataset = eval_mode.startswith("train")
         always_init_at_clip_start = eval_mode.endswith("start")
         prefix = eval_mode
         if os.getenv("LOCAL_RANK", "0") == "0":
-            Path(osp.join(output_dir, 'eval', prefix)).mkdir(parents=True, exist_ok=True)
-        snippets = (train_dataset.clip_snippets_flat if is_train_dataset
-                    else val_dataset.clip_snippets_flat if eval_mode.startswith("val")
-                    else FLAGS.extra_clips)
+            pth = Path(osp.join(output_dir, 'eval', prefix))
+            pth.mkdir(parents=True, exist_ok=True)
+        if is_train_dataset:
+            snippets = train_dataset.clip_snippets_flat
+        elif eval_mode.startswith("val"):
+            snippets = val_dataset.clip_snippets_flat
+        else:
+            snippets = FLAGS.extra_clips
         eval_callback = callbacks.PolicyEvaluationCallback(
             snippets,
             train_dataset.ref_steps,
@@ -228,28 +274,27 @@ def main(_):
             verbose=1
         )
         train_callbacks.append(eval_callback)
-    csv_logger = pl.loggers.CSVLogger(output_dir, name='logs', version='')
-    tb_logger = pl.loggers.TensorBoardLogger(output_dir, name='logs', version='')
-    gpus = -1 if FLAGS.gpus is None else [int(x) for x in FLAGS.gpus]
-    multigpu = (gpus == -1 and torch.cuda.device_count() > 1) or (gpus != -1 and len(gpus) > 1)
-    strategy = 'ddp' if multigpu else None
+    csv_logger = pl.loggers.CSVLogger(output_dir, name='logs')
+    tb_logger = pl.loggers.TensorBoardLogger(output_dir, name='logs')
+    val_check_int = FLAGS.validation_freq if val_loader is not None else None
+    # TODO: currently there's a bug when using GPU with RNNs
+    accelerator = "cpu" if is_rnn else 'gpu'
     trainer = pl.Trainer(
         default_root_dir=output_dir,
-        gpus=gpus,
-        auto_select_gpus=True,
-        strategy=strategy,
-        track_grad_norm=2 if FLAGS.track_grad_norm else -1,
+        accelerator=accelerator,
+        # track_grad_norm=2 if FLAGS.track_grad_norm else -1,
         max_steps=FLAGS.n_steps,
         max_time=timedelta(hours=FLAGS.n_hours),
         gradient_clip_val=FLAGS.max_grad_norm,
-        progress_bar_refresh_rate=FLAGS.progress_bar_refresh_rate,
-        val_check_interval=FLAGS.validation_freq if val_loader is not None else None,
+        val_check_interval=val_check_int,
         deterministic=True,
         benchmark=True,
         callbacks=train_callbacks,
         logger=[csv_logger, tb_logger]
     )
-    trainer.fit(policy, train_loader, val_dataloaders=val_loader, ckpt_path=FLAGS.load_path)
+    trainer.fit(policy, train_loader, val_dataloaders=val_loader,
+                ckpt_path=FLAGS.load_path)
+
 
 if __name__ == '__main__':
     app.run(main)
